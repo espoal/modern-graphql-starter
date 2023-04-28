ab05fd71305040879f4a076038d17fa954a1de40
espoal <alberto.websci@gmail.com>
2023-04-28 09:54:01 +0200

    feat: symlink

---
+++ tests/app.e2e.ts
@@ -0,0 +1,6 @@
+import test from 'node:test'
+import { strict as assert } from 'node:assert'
+
+test('e2e passing g', (t) => {
+    assert.strictEqual(1, 1)
+})
---
+++ tests/app.test.ts
@@ -0,0 +1,6 @@
+import test from 'node:test'
+import { strict as assert } from 'node:assert'
+
+test('passing unit in tests', (t) => {
+    assert.strictEqual(1, 1)
+})
